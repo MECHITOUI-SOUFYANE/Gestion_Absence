c0062580454679f5268180fa690b01bb91f0efbd
Hassanouthami <82836607+Hassanouthami@users.noreply.github.com>
2021-04-21 13:09:33 +0200

    Update ETUDIANT.java

--- src/main/java/entities/ETUDIANT.java
+++ src/main/java/entities/ETUDIANT.java
@@ -7,6 +7,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class ETUDIANT {
 	private @Id  Long NUM_APOGEE;
 	//pourquoi on besoin d un id module si lid de niveau
@@ -14,45 +16,6 @@ public class ETUDIANT {
 	  private String cne;
 	  private String nom;
 	  private String prenom;
-	  
-	  //constructeur
-	  ETUDIANT(){}
-	  ETUDIANT(Long apogee,String cne,String nom,String prenom,Long niveau){
-		  this.nom=nom;this.prenom=prenom;this.cne=cne;this.NUM_APOGEE=apogee;this.ID_niveau=niveau;
-	  }
-	  
-	  //getters and setters
-	public Long getNUM_APOGEE() {
-		return NUM_APOGEE;
-	}
-	public void setNUM_APOGEE(Long nUM_APOGEE) {
-		NUM_APOGEE = nUM_APOGEE;
-	}
-	public Long getID_niveau() {
-		return ID_niveau;
-	}
-	public void setID_niveau(Long iD_niveau) {
-		ID_niveau = iD_niveau;
-	}
-	public String getCne() {
-		return cne;
-	}
-	public void setCne(String cne) {
-		this.cne = cne;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public String getPrenom() {
-		return prenom;
-	}
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-	  
 	  
 	
 	 @Override
